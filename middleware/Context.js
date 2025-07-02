d98f13465899278f54364e809ccb714e5a69297b
Shubham Agrawal <shubham.agrawal@xebia.com>
2025-07-02 12:44:47 +0530

    nth commit commit

--- middleware/Context.js
+++ middleware/Context.js
@@ -17,7 +17,7 @@ const context = ({ req }) => {
   const body = req.body;
   const operationName = body.operationName;
 
-  console.log("endpoints: " + body);
+  console.log("endpoints: " + JSON.stringify(body));
 
   if (contextFreeOps.includes(operationName)) {
     return {}; // skip auth/context for public operations
